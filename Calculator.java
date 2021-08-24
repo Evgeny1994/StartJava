8e3d79a6e787f694d793344e503a9813de7e4663
Evgeny Myadelko <myadelko94@mail.ru>
2021-08-24 14:58:47 +0300

    first commit

--- Calculator.java
+++ Calculator.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
-public class Calculator{
+
+public class Calculator {
+
     private static  Scanner read;
-    public static void main (String[] args)
-    {
-    read = new Scanner(System.in);
-    int first;
-    int second;
-    String operator;
-    System.out.println("=");
-    first = read.nextInt();
-    operator = read.next();
-    second = read.nextInt();
-    if (operator.equals("+")) {
-        System.out.println("=" + (first + second));
-    }
-    if (operator.equals("-")) {
-        System.out.println("=" + (first - second));
-    }
-    if (operator.equals("*")) {
-        System.out.println("=" + (first * second));
-    }
-    if (operator.equals("/")) {
-        System.out.println("-" + (first / second));
-    }
-    if (operator.equals("^")) {
-        System.out.println("-" + (first * second));
-    }
-    if (operator.equals("%")) {
-        System.out.println("=" + (first % second));
-    }
+    
+    public static void main (String[] args) {
+        read = new Scanner(System.in);
+        int first;
+        int second;
+        String operator;
+        System.out.println(" = ");
+        first = read.nextInt();
+        operator = read.next();
+        second = read.nextInt();
+        if (operator.equals("+")) {
+            System.out.println(" = " + (first + second));
+        }
+        if (operator.equals(" - ")) {
+            System.out.println(" = " + (first - second));
+        }
+        if (operator.equals(" * ")) {
+            System.out.println(" = " + (first * second));
+        }
+        if (operator.equals(" / ")) {
+            System.out.println(" - " + (first / second));
+        }
+        if (operator.equals(" ^ ")) {
+            System.out.println(" - " + (first * second));
+        }
+        if (operator.equals(" % ")) {
+            System.out.println(" = " + (first % second));
+        }
     }
 }
