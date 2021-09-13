08e67f3ba5ad393c6b436c0cda0f8025ee3c84b7
Evgeny Myadelko <myadelko94@mail.ru>
2021-09-13 20:06:39 +0300

    first commit

--- Calculator.java
+++ Calculator.java
@@ -2,35 +2,30 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         int first;
         int second;
         String operator;
-        System.out.println(" = ");
         first = read.nextInt();
         operator = read.next();
         second = read.nextInt();
+        System.out.println("введите первое число, введите математический знак, введите второе число");
         if (operator.equals("+")) {
             System.out.println(" = " + (first + second));
-        }
-        else if (operator.equals(" - ")) {
+        } else if (operator.equals(" - ")) {
             System.out.println(" = " + (first - second));
-        }
-        else if (operator.equals(" * ")) {
+        } else if (operator.equals(" * ")) {
             System.out.println(" = " + (first * second));
-        }
-        else if (operator.equals(" / ")) {
+        } else if (operator.equals(" / ")) {
             System.out.println(" - " + (first / second));
-        }
-        else { 
+        } else if (operator.equals(" ^ ")) { 
             int result = 1;
-            for(int i = 1; i < second; i++) {
-            result*=first;
+                for(int i = 1; i < second; i++) {
+                result*=first;
         }
             System.out.println(" = " + result);
-        }
-        if (operator.equals(" % ")) {
+        } else if (operator.equals(" % ")) {
             System.out.println(" = " + (first % second));
         }
     }
