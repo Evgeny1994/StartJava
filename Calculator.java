c97b5167ad7b0b5bac41872d6e1ef2899221c968
Evgeny Myadelko <myadelko94@mail.ru>
2021-09-19 13:49:27 +0300

    first commit

--- Calculator.java
+++ Calculator.java
@@ -4,12 +4,12 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int first;
-        int second;
-        String operator;
-        first = read.nextInt();
-        operator = read.next();
-        second = read.nextInt();
+            System.out.println("введите первое число");
+        int first = read.nextInt();
+            System.out.println("введите математический знак");
+        String operator = read.next();
+            System.out.println("введите второе число");
+        int second = read.nextInt();
         System.out.println("введите первое число, введите математический знак, введите второе число");
         if (operator.equals("+")) {
             System.out.println(" = " + (first + second));
@@ -21,8 +21,8 @@ public class Calculator {
             System.out.println(" - " + (first / second));
         } else if (operator.equals(" ^ ")) { 
             int result = 1;
-                for(int i = 1; i < second; i++) {
-                result*=first;
+for(int i = 1; i < second; i++) {
+                result * =first;
             }
                 System.out.println(" = " + result);
         } else if (operator.equals(" % ")) {
